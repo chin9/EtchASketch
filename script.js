275d954770ad99fa744f5f5dfb78ce4f65dd0639
chin9 <chinchinjin@gmail.com>
2021-04-30 14:03:00 -0700

    size changing functionality and rainbow colours

--- script.js
+++ script.js
@@ -11,16 +11,70 @@ function makeRows(rows, cols) {
     }
 }
 
+
 function changeColor(element) {
-    element.style.backgroundColor = "black";
+    let num = Math.floor(Math.random() * 7)
+
+    switch (num) {
+        case 0: element.style.backgroundColor = "red";
+                break;
+
+        case 1: element.style.backgroundColor = "orange";
+                break;
+
+        case 2: element.style.backgroundColor = "yellow";
+                break;
+
+        case 3: element.style.backgroundColor = "green";
+                break;
+
+        case 4: element.style.backgroundColor = "blue";
+                break;
+
+        case 5: element.style.backgroundColor = "indigo";
+                break;
+
+        default: element.style.backgroundColor = "purple";
+                break;
+
+
+    }
+    // element.style.backgroundColor = "black";
+}
+
+function addEventListesnerForEachSquare(squares) {
+    for (let i = 0; i < squares.length; i ++) {
+        squares[i].addEventListener("mouseover", function() {
+            changeColor(squares[i]);
+            });
+    }
+}
+
+function clearAll(squares) {
+    for (let i = 0; i < squares.length; i ++) {
+        squares[i].style.backgroundColor = "white";
+    }
+}
+
+function deleteAll() {
+    container.innerHTML = '';
 }
 
 makeRows(16, 16);
 
 var squares = container.querySelectorAll('div');
 
-for (let i = 0; i < squares.length; i ++) {
-    squares[i].addEventListener("mouseover", function() {
-        changeColor(squares[i]);
-        });
-}
+addEventListesnerForEachSquare(squares);
+
+var clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", function() {
+    clearAll(squares);
+    var size = prompt("Enter a new size (max: 100):");
+    if (size > 100) {
+        var size = prompt("Enter a new size (max: 100):");
+    }
+    deleteAll();
+    makeRows(size, size);
+    squares = container.querySelectorAll('div');
+    addEventListesnerForEachSquare(squares);
+})
